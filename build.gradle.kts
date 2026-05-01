ff89ba833bc8e5718495cc35e56b15b27e8a5dd2
Sergio Fernández <sergiofdez2000@gmail.com>
2026-05-01 12:18:51 +0200

    build: add bundled kotlin plugin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         intellijIdea("2026.1.1")
+
+        bundledPlugin("org.jetbrains.kotlin")
+
         testFramework(TestFrameworkType.Platform)
     }
 }
