827859878faf5fec263b43f2ce836bf4ac9fd032
Sergio Fernández <sergiofdez2000@gmail.com>
2026-05-01 13:45:58 +0200

    build: upgrade intellij-platform plugin versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,14 @@ rootProject.name = "PsiKick"
 pluginManagement {
     plugins {
         id("org.jetbrains.kotlin.jvm") version "2.3.21"
-        id("org.jetbrains.intellij.platform") version "2.15.0"
+        id("org.jetbrains.intellij.platform") version "2.16.0"
         id("org.jetbrains.changelog") version "2.5.0"
     }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("org.jetbrains.intellij.platform.settings") version "2.14.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.16.0"
 }
 
 @Suppress("UnstableApiUsage")
